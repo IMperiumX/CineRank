5e31429ecbb118e180e05de157bcf418db7866fe
IMperiumX <yusufadell.dev@gmail.com>
2024-05-17 17:10:27 +0300

    feat: Add custom exceptions for movie module

---
+++ movies/exceptions.py
@@ -0,0 +1,14 @@
+class AuthenticationError(Exception):
+    pass
+
+
+class NotFoundError(Exception):
+    pass
+
+
+class APIError(Exception):
+    pass
+
+
+class ConnectionError(Exception):
+    pass

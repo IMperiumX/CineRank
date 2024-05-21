3730751a8e081251855e71d0c4793ed79750c289
IMperiumX <yusufadell.dev@gmail.com>
2024-05-21 19:40:55 +0300

    Add endpoint views

--- movies/api/views.py
+++ movies/api/views.py
@@ -1,9 +1,130 @@
-from rest_framework import viewsets
+from django_filters import rest_framework as filters
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from movies.models import Movie
-from movies.api.serializers import MovieSerializer
+from movies.models import (
+    Collection,
+    Genre,
+    Language,
+    Movie,
+    ProductCompany,
+    ProductCountry,
+)
+
+from .serializers import (
+    CollectionSerializer,
+    GenreSerializer,
+    LanguageSerializer,
+    MovieSerializer,
+    ProductCompanySerializer,
+    ProductCountrySerializer,
+)
 
 
-class MovieViewSet(viewsets.ModelViewSet):
+class GenreViewSet(ModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+    ordering = ["name"]
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CollectionViewSet(ModelViewSet):
+    queryset = Collection.objects.all()
+    serializer_class = CollectionSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+    ordering = ["name"]
+
+
+class LanguageViewSet(ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+    ordering = ["name"]
+
+
+class ProductCountryViewSet(ModelViewSet):
+    queryset = ProductCountry.objects.all()
+    serializer_class = ProductCountrySerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+    ordering = ["name"]
+
+
+class ProductCompanyViewSet(ModelViewSet):
+    queryset = ProductCompany.objects.all()
+    serializer_class = ProductCompanySerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["name"]
+    ordering_fields = ["name"]
+    ordering = ["name"]
+
+
+class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [filters.DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = [
+        "genre",
+        "production_company",
+        "production_country",
+        "collection",
+        "spoken_language",
+    ]
+    search_fields = ["title", "original_title"]
+    ordering_fields = ["title", "release_date"]
+    ordering = ["title"]
+
+    @action(detail=False, methods=["get"])
+    def get_user_recommendations(self, request, *args, **kwargs):
+        user_id = request.user.id
+        queryset = self.get_queryset().get_user_recommendations(user_id, count=100)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
